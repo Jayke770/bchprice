53de13abb822b7bbbd18c41631c11af12e17ae25
Jayke770 <jakevelasco40@gmail.com>
2022-11-05 17:19:22 +0800

    .

--- bot.js
+++ bot.js
@@ -2,18 +2,35 @@ import 'dotenv/config'
 import { Telegraf } from 'telegraf'
 import schedule from 'node-schedule'
 import CoinGecko from 'coingecko-api'
+import fs from 'fs-extra'
 const CoinGeckoClient = new CoinGecko()
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.launch().then(() => console.log("Bot Started"))
+bot.start(async (ctx) => {
+    const x = (await fs.readFile('db.txt', 'utf8'))
+    let data = x ? (await fs.readFile('db.txt', 'utf8')).split(',') : []
+    if (!data.includes(ctx.from.id.toString())) {
+        data.push(ctx.from.id)
+        await fs.writeFile('db.txt', data.toString())
+    }
+    await ctx.reply('This bot will send the BCH price every 30 minutes.')
+})
+bot.launch().then(async () => {
+    if (!(await fs.pathExists('db.txt'))) {
+        await fs.createFile('db.txt')
+    }
+    console.log('Bot Started')
+})
 const rule = new schedule.RecurrenceRule()
 rule.minute = 30
 rule.tz = 'Asia/Manila'
 schedule.scheduleJob(rule, async function () {
     await CoinGeckoClient.coins.fetch('bitcoin-cash').then(async (data) => {
-        let text = 'BCH Price\n\n'
-        text += `₱${data.data.market_data.current_price.php.toLocaleString()}\n`
-        await bot.telegram.sendMessage('1391502332', text)
+        const users = (await fs.readFile('db.txt', 'utf8')).split(',')
+        users.map(async (x) => {
+            let text = 'BCH Price\n\n'
+            text += `₱${data.data.market_data.current_price.php.toLocaleString()}\n`
+            await bot.telegram.sendMessage(x, text)
+        })
     })
 })
 // Enable graceful stop
